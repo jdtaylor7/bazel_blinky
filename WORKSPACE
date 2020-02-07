81f513c7f6013386c69c9e6a4e9dd070ef5ea349
Jordan Taylor <jdtaylor7@gmail.com>
2020-02-06 18:14:26 -0800

    Add initial Bazel files for cross compilation.

---
+++ WORKSPACE
@@ -0,0 +1,13 @@
+# Don't think this is necessary
+# load("bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+#
+# http_archive(
+#     name = "platforms",
+#     urls = [""],
+#     sha256 = "",
+#     build_file = "@//:BUILD",
+# )
+
+register_toolchains(
+
+)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "stm32f103xb.h"
 
-int main() {
+void mainFn() {
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
     GPIOC->CRH = 0b0100'0100'0011'0100'0100'0100'0100'0100;
     GPIOC->BRR = 1 << 13;
@@ -24,7 +24,32 @@ int main() {
     }
 }
 
-extern int (* const vectors[])() __attribute__ ((section(".vectors"))) = {
-                (int (*)())0x20000400,
-                main,
+/*
+ * Set up the vector table.
+ *
+ * "__attribute__ ((section(".vectors")))" defines a variable attribute which
+ * instructs GCC to emit this variable into the beginning of the .vectors
+ * section. This is later found by the linker.
+ *
+ * Type is vector of (void (*)()) because table is filled with (mostly) function
+ * pointers.
+ *
+ * Since this table never changes and is stored in flash, it's declared const.
+ *
+ * Const-qualification of a non-local non-volatile variable gives it internal
+ * linkage. To provide it with external linkage, declare as extern. This allows
+ * the structure to be accessed from other translation units.
+ *
+ * First entry contains the reset value of the stack pointer. Since the stack
+ * grows towards lower addresses and the start of SRAM is at 0x20000000,
+ * <address value> - 0x20000000 specifies the stack size.
+ *
+ * The second entry corresponds to the reset handler, which is where program
+ * execution starts once the microcontroller resets/powers on.
+ */
+typedef void (* const const_func_pointer_t)();
+extern const_func_pointer_t vectors[] __attribute__ ((section(".vectors"))) =
+{
+    (void (*)())0x20000400,
+    mainFn,
 };
